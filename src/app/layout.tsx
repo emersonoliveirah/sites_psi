c43fc27af883e19ad1accf9b7f7559bbb3da1b56
emersonoliveirah <emersonoliveirah@gmail.com>
2026-09-07 22:51:49 -0300

    Ajustes layout - Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,14 +42,14 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={`${fraunces.variable} ${karla.variable}`}>
-      <body className="flex min-h-screen flex-col">
+      <body className="flex min-h-dvh flex-col">
         <a href="#main" className="skip-link">
           Pular para o conteúdo
         </a>
         <Header />
         
         {/* Wrapper flex-1 adicionado para expandir o conteúdo e empurrar o footer */}
-        <div className="flex-1 w-full">
+        <div className="flex-1">
           {children}
         </div>
         
